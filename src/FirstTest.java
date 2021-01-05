f69057ebad656bfb61640fc52dd15e8d28e93f81
Dzmitry Viachaslavau <6870064@gmail.com>
2021-01-05 06:33:34 +0300

    01. Первый тест- find_element, click - completed,fixed

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,11 +1,9 @@
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -74,13 +72,13 @@ public class FirstTest {
         searchWikipediaClick();
         searchArticleTitle();
         clickArticleOpen();
-        swupeUp(2000);
-        swupeUp(2000);
-        swupeUp(2000);
-        swupeUp(2000);
-        swupeUp(2000);
-        swupeUp(2000);
-        swupeUp(2000);
+//        swupeUp(2000);
+//        swupeUp(2000);
+//        swupeUp(2000);
+//        swupeUp(2000);
+//        swupeUp(2000);
+//        swupeUp(2000);
+//        swupeUp(2000);
     }
 
     public void skipButtonClick(){
@@ -133,7 +131,7 @@ public class FirstTest {
     10);
 
     WebElement title_element = waitForTitleElement();
-    return title_element.getText();
+    return title_element.getAttribute("text");
     }
 
 //    WebElement title_element = waitForElementPresent(
@@ -208,15 +206,15 @@ public class FirstTest {
         return element;
     }
 
-    protected void swupeUp(int timeOfSwipe){
-        TouchAction action = new TouchAction(driver);
-        Dimension size = driver.manage().window().getSize();
-        int x = size.width / 2;
-        int start_y = (int) (size.height * 0.8);
-        int end_y = (int) (size.height * 0.2);
-
-        action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
-    }
+//    protected void swupeUp(int timeOfSwipe){
+//        TouchAction action = new TouchAction(driver);
+//        Dimension size = driver.manage().window().getSize();
+//        int x = size.width / 2;
+//        int start_y = (int) (size.height * 0.8);
+//        int end_y = (int) (size.height * 0.2);
+//
+//        action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
+//    }
 
 
 }
