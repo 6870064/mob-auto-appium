ba83b530298a89b6bb464410a26cdf0a8f67ad17
Dzmitry Viachaslavau <6870064@gmail.com>
2021-01-05 05:58:54 +0300

    01. Первый тест- find_element, click - completed

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,11 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -72,6 +74,13 @@ public class FirstTest {
         searchWikipediaClick();
         searchArticleTitle();
         clickArticleOpen();
+        swupeUp(2000);
+        swupeUp(2000);
+        swupeUp(2000);
+        swupeUp(2000);
+        swupeUp(2000);
+        swupeUp(2000);
+        swupeUp(2000);
     }
 
     public void skipButtonClick(){
@@ -198,6 +207,18 @@ public class FirstTest {
         element.clear();
         return element;
     }
+
+    protected void swupeUp(int timeOfSwipe){
+        TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+        int x = size.width / 2;
+        int start_y = (int) (size.height * 0.8);
+        int end_y = (int) (size.height * 0.2);
+
+        action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
+    }
+
+
 }
 
 
